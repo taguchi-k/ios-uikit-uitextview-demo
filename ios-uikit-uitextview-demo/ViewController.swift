7bb821eab416f460d664f442bc425139c95366dd
taguchi-k <k.taguchi@st-ventures.jp>
2017-05-11 01:59:44 +0900

    Add UITextViewのキーボード用ツールバーを使って閉じるボタン実装・Delegateメソッド実装

--- ios-uikit-uitextview-demo/ViewController.swift
+++ ios-uikit-uitextview-demo/ViewController.swift
@@ -8,51 +8,100 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-    @IBOutlet weak var textView: UITextView!
-    
+    @IBOutlet private weak var textView: UITextView!
+    @IBOutlet private weak var editableTextView: UITextView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+    }
+
+    /// setup系のメソッドを呼び出す
+    private func setup() {
         setupText()
         setupTextColor()
         setupTextFont()
         setupAlignment()
         setupEditable()
         setupDateDetectorType()
-        
+        setupEditableTextView()
     }
 
     // 表示するテキストを設定する
-    public func setupText(){
+    private func setupText(){
         textView.text = "testtesttesttesttest https://www.google.co.jp/ testtesttesttesttesttesttest 000-0000 testtesttesttesttesttesttesttesttesttesttesttest"
     }
 
     // 表示するテキストの色を設定する
-    public func setupTextColor(){
-        textView.textColor = UIColor.red
+    private func setupTextColor(){
+        textView.textColor = .red
     }
-    
+
     // 表示するテキストのフォントを設定する
-    public func setupTextFont(){
-        textView.font = UIFont.boldSystemFont(ofSize: 20)
+    private func setupTextFont(){
+        textView.font = .boldSystemFont(ofSize: 20)
     }
-    
+
     // 表示するテキストの位置を設定する
-    public func setupAlignment(){
-        textView.textAlignment = NSTextAlignment.center
+    private func setupAlignment(){
+        textView.textAlignment = .center
     }
-    
+
     // テキストを編集できるか設定する
-    public func setupEditable(){
+    private func setupEditable(){
         textView.isEditable = false
     }
-    
+
     // テキスト内に特定の文字がある場合、抽出してリンクに変換する
-    public func setupDateDetectorType(){
+    private func setupDateDetectorType(){
         textView.dataDetectorTypes = .all
     }
-    
 
+    // MARK: - UITextViewのキーボード用ツールバーを生成
+
+    /// キーボード上にツールバーを追加して閉じるボタンを実装する
+    private func setupEditableTextView() {
+
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+
+        // スペーサー
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        // 閉じる用のDoneボタン
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(didTapDoneButton(sender:)))
+        toolBar.items = [spacer, doneButton]
+
+        editableTextView.inputAccessoryView = toolBar
+    }
+
+    /// Doneボタン押下時に呼ばれる
+    @objc private func didTapDoneButton(sender: UIBarButtonItem) {
+        view.endEditing(true)
+    }
 }
 
+// MARK: - UITextViewDelegate
+extension ViewController: UITextViewDelegate {
+
+    // IB上で、入力可能なUITextViewのDelegate設定済み
+
+    // 編集開始直後に呼ばれる
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        print(#function)
+        print(textView.text)
+    }
+
+    // textViewのテキストまたは属性がユーザーによって変化したら呼ばれる
+    func textViewDidChange(_ textView: UITextView) {
+        print(#function)
+        print(textView.text)
+    }
+
+    // 編集終了直後に呼ばれる
+    func textViewDidEndEditing(_ textView: UITextView) {
+        print(#function)
+        print(textView.text)
+    }
+}
